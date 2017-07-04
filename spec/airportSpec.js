d8e3a4d8960ab1b4c1e80e20ef46a6598946a4e7
Miho Tse <mihotse@gmail.com>
2017-07-04 17:31:32 +0100

    Create plane

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,8 +1,13 @@
 describe("Airport", function() {
-  
+
+  beforeEach(function() {
+    plane = new Plane();
+  });
+
   it ("should land a plane", function() {
     var airport = new Airport();
-    airport.land();
+    airport.land(plane);
     expect(airport.planes.length).toEqual(1);
+    console.log(plane);
   });
 });
---
+++ src/plane.js
@@ -0,0 +1,3 @@
+var Plane = function() {
+  this.isAirborne = false;
+};
